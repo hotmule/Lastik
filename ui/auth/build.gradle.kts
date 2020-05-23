c8336120179b460af36d6726fedebe8c1252bac5
hotmule <ithr997@gmail.com>
2020-05-23 20:44:38 +0300

    Gradle update

--- ui/auth/build.gradle.kts
+++ ui/auth/build.gradle.kts
@@ -7,26 +7,10 @@ plugins {
 android {
 
     compileSdkVersion(Sdk.Version.compile)
-    buildToolsVersion(Sdk.Version.buildTools)
 
     defaultConfig {
-
-        applicationId = App.id
-
         minSdkVersion(Sdk.Version.min)
-        targetSdkVersion(Sdk.Version.target)
-
-        versionCode = App.Version.code
-        versionName = App.Version.name
-
         testInstrumentationRunner = Deps.Testing.androidJUnitRunner
-    }
-
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            proguardFiles(Proguard.rules)
-        }
     }
 }
 
