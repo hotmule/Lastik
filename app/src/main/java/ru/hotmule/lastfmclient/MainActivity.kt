05c4cd3fdd7ec58b100c6bcaf591ee39634c765e
hotmule <ithr997@gmail.com>
2020-05-31 23:01:43 +0300

    Edge-to-edge content set

--- app/src/main/java/ru/hotmule/lastfmclient/MainActivity.kt
+++ app/src/main/java/ru/hotmule/lastfmclient/MainActivity.kt
@@ -1,8 +1,11 @@
 package ru.hotmule.lastfmclient
 
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.core.content.ContextCompat
+import androidx.core.view.updatePadding
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -13,10 +16,40 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setEdgeToEdgeContent()
+        setBottomNavigation()
+    }
 
-        findViewById<BottomNavigationView>(R.id.bottom_navigation).setupWithNavController(
-            (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment)
-                .navController
-        )
+    private fun setEdgeToEdgeContent() {
+        window?.apply {
+            ContextCompat.getColor(context, android.R.color.transparent).also { transparent ->
+                navigationBarColor = transparent
+                statusBarColor = transparent
+            }
+            attributes = attributes?.apply {
+                flags = WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            }
+            decorView.apply {
+                systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                setOnApplyWindowInsetsListener { view, insets ->
+                    view.updatePadding(top = insets.stableInsetTop)
+                    insets
+                }
+            }
+        }
+    }
+
+    private fun setBottomNavigation() {
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).apply {
+            setupWithNavController(
+                (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment)
+                    .navController
+            )
+            setOnApplyWindowInsetsListener { view, insets ->
+                view.updatePadding(bottom = insets.stableInsetBottom)
+                insets
+            }
+        }
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,6 @@ rootProject.name = "LastFM-Client"
 include(
 
     Module.app,
-    Module.shared,
 
     Module.Ui.auth,
     Module.Ui.scrobbles,
